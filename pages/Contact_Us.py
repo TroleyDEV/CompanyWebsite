9451c7d93a28213c137b7ecc9de891248acda99c
Troley <troleyhs@gmail.com>
2024-03-07 17:48:34 +0100

    Small typo fixes and code formatting

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -3,22 +3,22 @@ import streamlit as st
 
 from send_email import send_email
 
-st.header("Contact Me")
+df = pandas.read_csv("topics.csv")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
 
-    # Select Box Topics
-    df = pandas.read_csv("topics.csv")
     topic = st.selectbox("Topic", options=df)
 
     raw_message = st.text_area("Your message")
+
     message = f"""\
 Subject: New email from {user_email}\n
 Topic: {topic}\n
 From: {user_email}\n
 {raw_message}
 """
+
     button = st.form_submit_button("Submit")
     if button:
         send_email(message, user_email)
